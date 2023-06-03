ebe2cc37dbbbddeb54d81bc10ef59671869e1909
facuud2 <facundocarri@hotmail.com.ar>
2023-06-03 18:35:10 -0300

    Finalizacion de validacion de formulario

--- js/script.js
+++ js/script.js
@@ -1,53 +1,160 @@
+// const formulario = document.getElementById('formularioJs');
+
+// const campoNombre = document.getElementById('nombre');
+
+// const campoApellido = document.getElementById('apellido');
+
+// const campoEmail = document.getElementById('email');
+
+// const campoHelp = document.getElementById('cajaTexto')
+
+// const alerta = document.getElementById('errorBlock')
+
+// const success = document.getElementById('successBlock')
+
+// formulario.addEventListener("submit", function(e) {
+//     e.preventDefault();
+
+//     if(!formulario) {
+
+//         if(campoNombre.value.trim() === ''){
+//             campoNombre.style.border = '1px solid #EF4444';
+//             alerta.style.display = 'block'
+    
+//             setTimeout(function() {
+//                 campoNombre.style.border = '1px solid #708090';
+//                 alerta.style.display = 'none'
+    
+//             }, 3600);
+//         }
+    
+//         if(campoApellido.value.trim() === ''){
+//             campoApellido.style.border = '1px solid #EF4444';
+//             alerta.style.display = 'block'
+    
+    
+//             setTimeout(function() {
+//                 campoApellido.style.border = '1px solid #708090';
+//                 alerta.style.display = 'none'
+    
+//             }, 3600);
+//         }
+    
+//         if(campoEmail.value.trim() === ''){
+//             campoEmail.style.border = '1px solid #EF4444';
+//             alerta.style.display = 'block'
+    
+    
+//             setTimeout(function() {
+//                 campoEmail.style.border = '1px solid #708090';
+//                 alerta.style.display = 'none'
+    
+//             }, 3600);
+//         }
+    
+//         if(cajaTexto.value.trim() === ''){
+//             cajaTexto.style.border = '1px solid #EF4444';
+//             alerta.style.display = 'block';
+    
+    
+//             setTimeout(function() {
+//                 cajaTexto.style.border = '1px solid #708090';
+//                 alerta.style.display = 'none'
+    
+//             }, 3600);
+//         }
+//     }
+
+
+//     if(formulario){
+//         success.style.display = 'block'
+
+//         setTimeout(function() {
+//             success.style.display = 'none'
+//         },3600)
+//     }
+// }
+// );
+
 const formulario = document.getElementById('formularioJs');
-
 const campoNombre = document.getElementById('nombre');
-
 const campoApellido = document.getElementById('apellido');
-
 const campoEmail = document.getElementById('email');
+const campoHelp = document.getElementById('cajaTexto');
+const alerta = document.getElementById('errorBlock');
+const success = document.getElementById('successBlock');
 
-const campoHelp = document.getElementById('cajaTexto')
+function todosLosCamposSonValidos() {
+    if (campoNombre.value.trim() === '') {
+        return false;
+    }
+
+    if (campoApellido.value.trim() === '') {
+        return false;
+    }
+
+    if (campoEmail.value.trim() === '') {
+        return false;
+    }
+
+    if (campoHelp.value.trim() === '') {
+        return false;
+    }
+
+    return true;
+}
 
 formulario.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    
-
-    if(campoNombre.value.trim() === ''){
-        campoNombre.style.border = '1px solid #EF4444'
+    if (campoNombre.value.trim() === '') {
+        campoNombre.style.border = '1px solid #EF4444';
+        alerta.style.display = 'block';
 
         setTimeout(function() {
             campoNombre.style.border = '1px solid #708090';
-        }, 1500);
+            alerta.style.display = 'none';
+        }, 3600);
     }
 
-    if(campoApellido.value.trim() === ''){
-        campoApellido.style.border = '1px solid #EF4444'
+    if (campoApellido.value.trim() === '') {
+        campoApellido.style.border = '1px solid #EF4444';
+        alerta.style.display = 'block';
 
         setTimeout(function() {
             campoApellido.style.border = '1px solid #708090';
-        }, 1500);
+            alerta.style.display = 'none';
+        }, 3600);
     }
 
-    if(campoEmail.value.trim() === ''){
-        campoEmail.style.border = '1px solid #EF4444'
+    if (campoEmail.value.trim() === '') {
+        campoEmail.style.border = '1px solid #EF4444';
+        alerta.style.display = 'block';
 
         setTimeout(function() {
             campoEmail.style.border = '1px solid #708090';
-        }, 1500);
+            alerta.style.display = 'none';
+        }, 3600);
     }
 
-    if(cajaTexto.value.trim() === ''){
-        cajaTexto.style.border = '1px solid #EF4444'
+    if (campoHelp.value.trim() === '') {
+        campoHelp.style.border = '1px solid #EF4444';
+        alerta.style.display = 'block';
 
         setTimeout(function() {
-            cajaTexto.style.border = '1px solid #708090';
-        }, 1500);
+            campoHelp.style.border = '1px solid #708090';
+            alerta.style.display = 'none';
+        }, 3600);
     }
 
-    campoNombre.value = ''
-    campoApellido.value = ''
-    campoEmail.value = ''
-    campoHelp.value = ''
-}
-);
+    if (todosLosCamposSonValidos()) {
+        success.style.display = 'block';
+        campoNombre.value = ''
+        campoApellido.value = ''
+        campoHelp.value = ''
+
+        setTimeout(function() {
+            success.style.display = 'none';
+        }, 3600);
+    }
+});
